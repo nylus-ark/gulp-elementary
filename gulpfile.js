01b076e863e7b2e4679bf8f63ec9bfc5339fbe1c
Ivan Smirnov <the.ivan.smirnov@gmail.com>
2020-02-10 13:57:42 +0400

    Bag fixes

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,11 @@ async function js(src, dest) {
   return await gulp
     .src(src)
     .pipe(concat("script.js"))
-    .pipe(uglify({ toplevel: true }))
+    .pipe(
+      uglify({ toplevel: true }).on("error", function() {
+        this.emit("end");
+      })
+    )
     .pipe(gulp.dest(dest))
     .pipe(browserSync.reload({ stream: true }));
 }
